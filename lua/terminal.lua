28d3fceb4e8892889afaced075f823d4556ca427
Luxed <devildead13@gmail.com>
2019-11-26 18:23:13 -0500

    Over-engineered terminal.lua (for the sake of learning lua)

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,18 +1,43 @@
 local list_of_terms = {}
 
-local function Named(name, command)
-    -- Add named terminal to list
-    if list_of_terms[name] and vim.api.nvim_buf_is_loaded(list_of_terms[name]) then
-        -- Open named terminal
-        vim.api.nvim_set_current_buf(list_of_terms[name])
+local Terminal = {}
+Terminal.__index = Terminal
+
+function Terminal.create(name, command)
+    local term = setmetatable({}, Terminal)
+
+    term.name = name
+    term.command = command
+    term.buffer = nil
+
+    return term
+end
+
+function Terminal:is_loaded()
+    return self.buffer and vim.api.nvim_buf_is_loaded(self.buffer)
+end
+
+function Terminal:switch_to_buffer()
+    if self:is_loaded() then
+        vim.api.nvim_set_current_buf(self.buffer)
     else
         local buf = vim.api.nvim_create_buf(false, false)
 
         vim.api.nvim_set_current_buf(buf)
-        vim.api.nvim_call_function("termopen", {command})
+        vim.api.nvim_call_function("termopen", {self.command})
 
-        list_of_terms[name] = buf
+        self.buffer = buf
     end
+end
+
+local function Named(name, command)
+    if not list_of_terms[name] then
+        local term = Terminal.create(name, command)
+
+        list_of_terms[name] = term
+    end
+
+    list_of_terms[name]:switch_to_buffer()
 end
 
 return {
