c1ec6a30fbfd9878747993c99b4763d3f03ab161
Luxed <devildead13@gmail.com>
2021-01-17 00:28:53 -0500

    Fix indentation with new lua plugin + make zS mapping silent

--- lua/00_vanilla.lua
+++ lua/00_vanilla.lua
@@ -4,56 +4,56 @@ local command = require('helpers.command').command
 
 local default_indent = 4
 set('assign', {
-    b = {
-      tabstop = default_indent,
-      softtabstop = default_indent,
-      expandtab = true,
-      shiftwidth = default_indent,
-      -- TODO: Look into how the following works:
-      -- smartindent = true
-      autoindent = true
-    },
-    w = {
-      wrap = true,
-      linebreak = true,
-      breakindent = true,
-      breakindentopt = 'shift:2',
-      number = true,
-      cursorline = true,
-      foldmethod = 'syntax',
-      signcolumn = 'yes'
-    },
-    o = {
-      showbreak = '> ',
-      wildmenu = true,
-      showcmd = true,
-      lazyredraw = true,
-      showmatch = true,
-      incsearch = true,
-      hlsearch = false,
-      ruler = true,
-      laststatus = 2,
-      confirm = true,
-      visualbell = true,
-      mouse = 'a',
-      cmdheight = 2,
-      inccommand = 'nosplit',
-      splitbelow = true,
-      splitright = true,
-      autowrite = true,
-      updatetime = 300,
-      termguicolors = true,
-      grepprg = 'rg --no-heading --vimgrep --smart-case',
-      hidden = true,
-    }
-  })
+  b = {
+    tabstop = default_indent,
+    softtabstop = default_indent,
+    expandtab = true,
+    shiftwidth = default_indent,
+    -- TODO: Look into how the following works:
+    -- smartindent = true
+    autoindent = true
+  },
+  w = {
+    wrap = true,
+    linebreak = true,
+    breakindent = true,
+    breakindentopt = 'shift:2',
+    number = true,
+    cursorline = true,
+    foldmethod = 'syntax',
+    signcolumn = 'yes'
+  },
+  o = {
+    showbreak = '> ',
+    wildmenu = true,
+    showcmd = true,
+    lazyredraw = true,
+    showmatch = true,
+    incsearch = true,
+    hlsearch = false,
+    ruler = true,
+    laststatus = 2,
+    confirm = true,
+    visualbell = true,
+    mouse = 'a',
+    cmdheight = 2,
+    inccommand = 'nosplit',
+    splitbelow = true,
+    splitright = true,
+    autowrite = true,
+    updatetime = 300,
+    termguicolors = true,
+    grepprg = 'rg --no-heading --vimgrep --smart-case',
+    hidden = true,
+  }
+})
 
 set('increment', {
-    o = {
-      shortmess = 'c',
-      diffopt = ',algorithm:patience'
-    }
-  })
+  o = {
+    shortmess = 'c',
+    diffopt = ',algorithm:patience'
+  }
+})
 
 vim.g.mapleader = ','
 
@@ -65,7 +65,7 @@ map.nnore('<leader>bq', ':b# <BAR> bd #<CR>')
 map.nnore('<leader>ei', ':e $MYVIMRC<CR>')
 map.nnore('<leader>si', ':source $MYVIMRC<CR>')
 
-map.nnore('zS', 'lua print(require("helpers.syntax").getCursorHightlights())')
+map.nnore('zS', ':lua print(require("helpers.syntax").getCursorHighlights())<CR>', {silent = true})
 
 -- toggle highlight
 map.nnore('<leader>th', ':set hlsearch!<CR>')
