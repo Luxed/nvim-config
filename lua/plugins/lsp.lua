171fe052aa27287384231ed21acba30f2d3eddd7
Luxed <devildead13@gmail.com>
2021-01-23 23:18:35 -0500

    Add lsp_status initialization config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,17 @@
 local nvim_lsp = require('lspconfig')
 local au = require('helpers.command').autocmd
 local map = require('helpers.map')
+local lsp_status = require('lsp-status')
 
-local function on_attach_au()
-  au('CursorHold',  '<buffer>', 'lua vim.lsp.buf.document_highlight()')
-  au('CursorHoldI', '<buffer>', 'lua vim.lsp.buf.document_highlight()')
-  au('CursorMoved', '<buffer>', 'lua vim.lsp.buf.clear_references()')
+local function on_attach_au(client)
+  if client.resolved_capabilities.document_highlight then
+    au('CursorHold',  '<buffer>', 'lua vim.lsp.buf.document_highlight()')
+    au('CursorHoldI', '<buffer>', 'lua vim.lsp.buf.document_highlight()')
+    au('CursorMoved', '<buffer>', 'lua vim.lsp.buf.clear_references()')
+  end
 end
 
--- lsp (builtin)
+-- TODO use buffer number from LSP to bind keys
 local function on_attach_keymaps()
   local silent_opts = {silent = true}
   map.nnore('<leader>qk', '<cmd>lua vim.lsp.buf.hover()<CR>', silent_opts, true)
@@ -33,9 +36,11 @@ local function on_attach_keymaps()
   vim.api.nvim_buf_set_keymap(0, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
 end
 
-local function on_attach_complete()
-  on_attach_au()
+local function on_attach_complete(client)
+  on_attach_au(client)
   on_attach_keymaps()
+
+  lsp_status.on_attach(client)
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -48,12 +53,18 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
     update_in_insert = false
   })
 
-nvim_lsp.vuels.setup{on_attach = on_attach_complete}
-nvim_lsp.rls.setup{on_attach = on_attach_complete}
-nvim_lsp.tsserver.setup{on_attach = on_attach_complete}
-nvim_lsp.vimls.setup{on_attach = on_attach_complete}
-nvim_lsp.html.setup{on_attach = on_attach_complete}
---nvim_lsp.jdtls.setup{on_attach = on_attach_complete}
+local complete_lsp_setup = {
+  on_attach = on_attach_complete,
+  capabilities = lsp_status.capabilities
+}
+
+nvim_lsp.vuels.setup(complete_lsp_setup)
+nvim_lsp.rls.setup(complete_lsp_setup)
+nvim_lsp.tsserver.setup(complete_lsp_setup)
+nvim_lsp.vimls.setup(complete_lsp_setup)
+nvim_lsp.html.setup(complete_lsp_setup)
+--nvim_lsp.jdtls.setup(complete_lsp_setup)
+require('nlua.lsp.nvim').setup(nvim_lsp, complete_lsp_setup)
 
 -- TODO: make this work for Linux and Windows
 local pid = vim.fn.getpid()
@@ -61,13 +72,10 @@ local home = vim.fn.expand('~')
 local omnisharp_bin = home .. '/.local/opt/omnisharp-server/run'
 nvim_lsp.omnisharp.setup{
   cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) },
-  on_attach = on_attach_keymaps
+  on_attach = on_attach_complete
 }
 
-require('nlua.lsp.nvim').setup(nvim_lsp, {on_attach = on_attach_complete})
-
-vim.cmd('sign define LspDiagnosticsSignError text=')
-vim.cmd('sign define LspDiagnosticsSignWarning text=')
-vim.cmd('sign define LspDiagnosticsSignInformation text=')
-vim.cmd('sign define LspDiagnosticsSignHint text=ﯦ')
-
+vim.fn.sign_define('LspDiagnosticsSignError', { text = '' })
+vim.fn.sign_define('LspDiagnosticsSignWarning', { text='' })
+vim.fn.sign_define('LspDiagnosticsSignInformation', { text='' })
+vim.fn.sign_define('LspDiagnosticsSignHint', { text='ﯦ' })
