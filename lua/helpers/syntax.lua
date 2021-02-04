8ddb965f45b7929176a6a558cbe6c54707913862
Luxed <devildead13@gmail.com>
2021-02-04 13:31:10 -0500

    Improve "getCursorHighlights" function to give playground results when everything works

--- lua/helpers/syntax.lua
+++ lua/helpers/syntax.lua
@@ -1,7 +1,18 @@
 return {
   getCursorHighlights = function()
-    local synstack = vim.fn.synstack(vim.fn.line('.'), vim.fn.col('.'))
-    local map = vim.fn.map(synstack, 'synIDattr(v:val, "name")')
-    return vim.fn.join(vim.fn.reverse(map), ' ')
+    local ft = vim.bo.filetype
+    local ts_ft = ft
+    if ts_ft == 'cs' then
+      ts_ft = 'c_sharp'
+    end
+
+    local is_ts_enabled = require('nvim-treesitter.configs').is_enabled('highlight', ts_ft) and require('nvim-treesitter.configs').is_enabled('playground', ts_ft)
+    if is_ts_enabled then
+      require('nvim-treesitter-playground.hl-info').show_hl_captures() 
+    else
+      local synstack = vim.fn.synstack(vim.fn.line('.'), vim.fn.col('.'))
+      local map = vim.fn.map(synstack, 'synIDattr(v:val, "name")')
+      return vim.fn.join(vim.fn.reverse(map), ' ')
+    end
   end
 }
