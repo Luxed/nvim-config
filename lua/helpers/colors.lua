ed80844d33fec2fee26a88f79b9fd12a6ef9fc90
Luxed <devildead13@gmail.com>
2021-02-04 14:14:47 -0500

    Add get_hi_attr function to colors helper module

--- lua/helpers/colors.lua
+++ lua/helpers/colors.lua
@@ -1,20 +1,21 @@
--- TODO: find if there's a better way to do this using vim functions
-local function hi(group, options)
-  local cmd = 'hi! ' .. group .. ' '
-
-  for k,v in pairs(options) do
-    cmd = cmd .. k .. '=' .. v .. ' '
-  end
-
-  vim.cmd(cmd)
-end
-
-local function hi_link(group, link_group)
-  vim.cmd('hi! link ' .. group .. ' ' .. link_group)
-end
-
-
 return {
-  hi = hi,
-  hi_link = hi_link
+  hi = function(group, options)
+    local cmd = 'hi! ' .. group .. ' '
+
+    for k,v in pairs(options) do
+      cmd = cmd .. k .. '=' .. v .. ' '
+    end
+
+    vim.cmd(cmd)
+  end,
+  hi_link = function(group, link_group)
+    vim.cmd('hi! link ' .. group .. ' ' .. link_group)
+  end,
+  -- See `:h synIDattr()` for more info
+  get_hi_attr = function(hi_name, attr)
+    local hl_id = vim.fn.hlID(hi_name)
+    local hl_hex_color = vim.fn.synIDattr(hl_id, attr)
+
+    return hl_hex_color
+  end
 }
