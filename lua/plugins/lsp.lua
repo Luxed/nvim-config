bcdbd9ef7d8ccca33534a4f8c4ac091d2ab43c3c
Luxed <devildead13@gmail.com>
2021-02-08 15:19:14 -0500

    Improve lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,13 +4,9 @@ local map = require('helpers.map')
 local lsp_status = require('lsp-status')
 local root_pattern = require('lspconfig.util').root_pattern
 
--- TODO: Investigate why highlighting is _really_ slow with the typescript-language-server specifically
-local highlight_blacklist = {
-  --typescript = true
-}
-
 local function on_attach_au(client)
-  if client.resolved_capabilities.document_highlight and not highlight_blacklist[vim.bo.filetype] then
+  -- NOTE: Some lsp (like omnisharp for example) will _crash_ instead of doing nothing when asked for highlight
+  if client.resolved_capabilities.document_highlight then
     augroup('lsp_document_highlight', {
         { 'CursorHold', '<buffer>', 'lua vim.lsp.buf.document_highlight()' },
         { 'CursorMoved', '<buffer>', 'lua vim.lsp.buf.clear_references()' }
@@ -25,11 +21,9 @@ local function on_attach_keymaps()
   map.nnore('K', '<cmd>lua vim.lsp.buf.hover()<CR>', silent_opts, true)
   map.nnore('<leader>qK', '<cmd>lua vim.lsp.buf.signature_help()<CR>', silent_opts, true)
   map.nnore('<leader>qq', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', silent_opts, true)
-  --map.nnore('<leader>qgr', '<cmd>lua vim.lsp.buf.references()<CR>', silent_opts, true)
   map.nnore('<leader>qgd', '<cmd>lua vim.lsp.buf.definition()<CR>', silent_opts, true)
   map.nnore('<leader>qgi', '<cmd>lua vim.lsp.buf.implementation()<CR>', silent_opts, true)
   map.nnore('<leader>qr', '<cmd>lua vim.lsp.buf.rename()<CR>', {}, true)
-  --map.nnore('<leader>qa', '<cmd>lua vim.lsp.buf.code_action()<CR>', {}, true)
   map.nnore('<leader>qn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {}, true)
   map.nnore('<leader>qp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', {}, true)
   map.nnore('<leader>qd', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', {}, true)
@@ -68,7 +62,9 @@ local complete_lsp_setup = {
   capabilities = lsp_capabilities
 }
 
+-- NOTE: On windows, some commands created by npm will not work unless you explicitely tell lspconfig to use the ".cmd" executable
 nvim_lsp.vuels.setup(vim.tbl_extend('force', complete_lsp_setup, {
+      cmd = vim.g.vls_cmd,
       init_options = {
         config = {
           vetur = {
@@ -79,11 +75,13 @@ nvim_lsp.vuels.setup(vim.tbl_extend('force', complete_lsp_setup, {
         }
       }
   }))
+nvim_lsp.cssls.setup(vim.tbl_extend('force', complete_lsp_setup, {
+      cmd = vim.g.cssls_cmd
+  }))
 nvim_lsp.rls.setup(complete_lsp_setup)
 nvim_lsp.tsserver.setup(complete_lsp_setup)
 nvim_lsp.vimls.setup(complete_lsp_setup)
 nvim_lsp.html.setup(complete_lsp_setup)
---nvim_lsp.jdtls.setup(complete_lsp_setup)
 require('nlua.lsp.nvim').setup(nvim_lsp, complete_lsp_setup)
 
 if vim.g.config_omnisharp_bin then
@@ -99,7 +97,7 @@ if vim.g.config_omnisharp_bin then
   }
 end
 
-vim.fn.sign_define('LspDiagnosticsSignError', { text = '' })
-vim.fn.sign_define('LspDiagnosticsSignWarning', { text='' })
+vim.fn.sign_define('LspDiagnosticsSignError'      , { text='' })
+vim.fn.sign_define('LspDiagnosticsSignWarning'    , { text='' })
 vim.fn.sign_define('LspDiagnosticsSignInformation', { text='' })
-vim.fn.sign_define('LspDiagnosticsSignHint', { text='ﯦ' })
+vim.fn.sign_define('LspDiagnosticsSignHint'       , { text='ﯦ' })
