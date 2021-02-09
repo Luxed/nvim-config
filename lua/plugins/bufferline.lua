85786e01cf0eb98212efb60f43528309af604a97
Luxed <devildead13@gmail.com>
2021-02-08 22:55:58 -0500

    Remove custom bufferline colors

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,67 +1,9 @@
 local bufferline = require('bufferline')
-local ayu_get_color = vim.fn['ayu#get_color']
-
--- TODO: look into fixing ayu-vim directly
-local function custom_highlights()
-  if vim.g.colors_name ~= 'ayu' then
-    return nil
-  end
-
-  return {
-    fill = {
-      guifg = '',
-      guibg = ayu_get_color('panel_border')
-    },
-    buffer_selected = {
-      guibg = ayu_get_color('panel_bg')
-    },
-    buffer_visible = {
-      guibg = ayu_get_color('panel_shadow')
-    },
-    separator_selected = {
-      guifg = ayu_get_color('panel_border'),
-      guibg = ayu_get_color('panel_bg')
-    },
-    separator_visible = {
-      guifg = ayu_get_color('panel_border'),
-      guibg = ayu_get_color('panel_shadow')
-    },
-    separator = {
-      guifg = ayu_get_color('panel_border')
-    },
-    duplicate_selected = {
-      guibg = '',
-    },
-    duplicate_visible = {
-      guibg = ''
-    },
-    modified_selected = {
-      guibg = ayu_get_color('panel_bg')
-    },
-    modified_visible = {
-      guibg = ayu_get_color('panel_shadow')
-    },
-    warning_selected = {
-      guibg = ayu_get_color('panel_bg')
-    },
-    warning_visible = {
-      guibg = ayu_get_color('panel_shadow')
-    },
-    error_selected = {
-      guibg = ayu_get_color('panel_bg'),
-      guifg = ayu_get_color('error')
-    },
-    error_visible = {
-      guibg = ayu_get_color('panel_shadow')
-    }
-  }
-end
 
 return {
   init = function()
     bufferline.setup{
       options = {
-        --numbers = 'ordinal',
         show_buffer_close_icons = false,
         separator_style = 'slant',
         diagnostics = 'nvim_lsp',
@@ -69,8 +11,7 @@ return {
           local icon = level:match("error") and "" or ""
           return " " .. icon .. count
         end,
-      },
-      highlights = custom_highlights()
+      }
     }
   end
 }
