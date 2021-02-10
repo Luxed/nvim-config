d3643bc5b7a2db50a1af825893235eba34d7333c
Luxed <devildead13@gmail.com>
2021-02-09 19:49:07 -0500

    Add html to TreeSitter list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c_sharp', 'css', 'javascript', 'lua', 'rust' },
+  ensure_installed = { 'c_sharp', 'css', 'html', 'javascript', 'lua', 'rust' },
   highlight = {
     enable = true,
   },
