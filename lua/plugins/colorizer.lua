a97bdabbba0d7f53a869f0224055ba6877dd257d
Luxed <devildead13@gmail.com>
2021-02-17 15:36:46 -0500

    Add css to colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,11 +1,14 @@
 require('colorizer').setup({
-    dosini = {
-      names = false,
-      mode = 'background',
+    css = {
+      names = true
     },
     cfg = {
       names = false,
       RRGGBBAA = true,
+    },
+    dosini = {
+      names = false,
+      mode = 'background',
     },
     lua = {
       names = false,
