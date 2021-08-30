7cabc79b40d8f610d0f30074aa3463dda1541032
Luxed <devildead13@gmail.com>
2021-08-30 15:57:03 -0400

    Remove print() + add ftdetect for systemd's service files

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,17 +18,16 @@ cmp.setup{
       name = 'buffer',
       opts = {
         get_bufnrs = function()
-          print('testing if it works from here')
-            local bufs = {}
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              bufs[vim.api.nvim_win_get_buf(win)] = true
-            end
-            return vim.tbl_keys(bufs)
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
           end
-        },
+          return vim.tbl_keys(bufs)
+        end
       },
-      { name = 'path' },
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }
-    }
+    },
+    { name = 'path' },
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }
   }
+}
