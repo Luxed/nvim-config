323e79e5bee1b0c8dcfcb727c36123f0c44e2abd
Luxed <devildead13@gmail.com>
2021-10-21 13:46:05 -0400

    Update indent_blankline config

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,9 +1,10 @@
 vim.cmd('packadd indent-blankline.nvim')
 
-vim.g.indent_blankline_show_first_indent_level = false
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_filetype_exclude = { 'help' }
-
--- TreeSitter related settings
-vim.g.indent_blankline_use_treesitter = false
-vim.g.indent_blankline_show_current_context = true
+require('indent_blankline').setup {
+  buftype_exclude = {"terminal", "help"},
+  char_list = {'|', '¦', '┆', '┊'},
+  use_treesitter = false,
+  show_first_indent_level = false,
+  show_trailing_blankline_indent = false,
+  show_current_context = true,
+}
