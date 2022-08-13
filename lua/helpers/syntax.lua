a0bf4b957b8ee792a75193d0571e91a40a15d32a
Luxed <devildead13@gmail.com>
2022-08-13 00:43:01 -0400

    Add csharp cursor highlight

--- lua/helpers/syntax.lua
+++ lua/helpers/syntax.lua
@@ -1,5 +1,10 @@
 return {
   showCursorHighlights = function()
-    require('nvim-treesitter-playground.hl-info').show_hl_captures()
+    local ft = vim.bo.filetype
+    if ft == 'cs' then
+      require('omnisharp').show_highlights_under_cursor()
+    else
+      require('nvim-treesitter-playground.hl-info').show_hl_captures()
+    end
   end
 }
