d121f1e970bf0e55617ef41bbff2e6780ba6c591
Luxed <devildead13@gmail.com>
2022-11-09 20:03:26 -0500

    Update dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,25 +3,36 @@ local dap, dapui, dap_virtual_text = require('dap'), require('dapui'), require('
 dapui.setup{}
 
 dap.listeners.after.event_initialized['dapui_config'] = function()
-  dapui.open()
+  dapui.open({})
 end
 dap.listeners.before.event_terminated['dapui_config'] = function()
-  dapui.close()
+  dapui.close({})
 end
 dap.listeners.before.event_exited['dapui_config'] = function()
-  dapui.close()
+  dapui.close({})
 end
 
-dap_virtual_text.setup()
+dap_virtual_text.setup({})
 
 require('keymaps').dap()
 
-local mason_path = vim.fn.stdpath('data') .. '/mason'
+--[[ local mason_path = vim.fn.stdpath('data') .. '/mason'
 local netcoredbg_path = mason_path .. '/bin/netcoredbg'
 dap.adapters.coreclr = {
   type = 'executable',
   command = netcoredbg_path,
   args = {'--interpreter=vscode'}
+} ]]
+
+dap.configurations.cs = {
+  {
+    name = "Attach to process",
+    type = 'coreclr',
+    request = 'attach',
+    -- TODO: I _think_ the process could be found automatically using a similar method to the automatic
+    processId = require('dap.utils').pick_process,
+    args = {}
+  }
 }
 
 vim.fn.sign_define('DapBreakpoint', { text='', texthl='DiagnosticDefaultError' })
