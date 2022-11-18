91616d87b38de91be58292bbb118f5de0b718a94
Luxed <devildead13@gmail.com>
2022-11-18 11:42:31 -0500

    Show trailing whitespace and mixed indentation in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,6 +10,46 @@ local aw_section = {
   end
 }
 
+local mixed_indent_section = {
+  function()
+    local space_pat = [[\v^ +]]
+    local tab_pat = [[\v^\t+]]
+    local space_indent = vim.fn.search(space_pat, 'nwc')
+    local tab_indent = vim.fn.search(tab_pat, 'nwc')
+    local mixed = (space_indent > 0 and tab_indent > 0)
+    local mixed_same_line
+
+    if not mixed then
+      mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+      mixed = mixed_same_line > 0
+    end
+
+    if not mixed then
+      return ''
+    end
+
+    if mixed_same_line ~= nil and mixed_same_line > 0 then
+      return 'MI:' .. mixed_same_line
+    end
+
+    local space_indent_cnt = vim.fn.searchcount({ pattern = space_pat, max_count = 1e3 }).total
+    local tab_indent_cnt = vim.fn.searchcount({ pattern = tab_pat, max_count = 1e3 }).total
+
+    if space_indent_cnt > tab_indent_cnt then
+      return 'MI:' .. tab_indent
+    else
+      return 'MI:' .. space_indent
+    end
+  end
+}
+
+local trailing_whitespace_section = {
+  function()
+    local space = vim.fn.search([[\s\+$]], 'nwc')
+    return space ~= 0 and "TW:"..space or ""
+  end
+}
+
 local winbar = {
   lualine_c = {
     {
@@ -36,6 +76,11 @@ require('lualine').setup({
       'fileformat',
       'filetype',
       aw_section,
+    },
+    lualine_z = {
+      trailing_whitespace_section,
+      mixed_indent_section,
+      'location'
     }
   },
   winbar = winbar,
