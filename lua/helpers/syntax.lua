4238410efa615db3d8f6278b6c046d705a8dc60a
Luxed <devildead13@gmail.com>
2022-12-28 11:42:07 -0500

    Support Neovim's new "Inspect" command

--- lua/helpers/syntax.lua
+++ lua/helpers/syntax.lua
@@ -1,10 +1,14 @@
 return {
   showCursorHighlights = function()
-    local ft = vim.bo.filetype
-    if ft == 'cs' then
-      require('omnisharp').show_highlights_under_cursor()
+    if vim.fn.has('nvim-0.9') == 1 then
+      vim.show_pos()
     else
-      require('nvim-treesitter-playground.hl-info').show_hl_captures()
+      local ft = vim.bo.filetype
+      if ft == 'cs' then
+        require('omnisharp').show_highlights_under_cursor()
+      else
+        require('nvim-treesitter-playground.hl-info').show_hl_captures()
+      end
     end
   end
 }
