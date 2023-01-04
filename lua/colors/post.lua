57cee3a7c195fa684ae2509acddeb6a1762dabea
Luxed <devildead13@gmail.com>
2023-01-04 13:36:09 -0500

    Properly highlight "@type.qualifier"

--- lua/colors/post.lua
+++ lua/colors/post.lua
@@ -17,6 +17,7 @@ return {
         hi_link('@struct', 'OmniSharpStructName')
         hi_link('@interface', 'OmniSharpInterfaceName')
         hi_link('@enum', 'OmniSharpEnumName')
+        hi_link('@type.qualifier', 'Keyword')
       end,
       ['afterglow'] = function()
         -- Set pmenu to complete black for better Fzf highlighting
