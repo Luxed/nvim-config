08992c268d49bd1d1f3225a8f585e5fef22b1513
Luxed <devildead13@gmail.com>
2023-01-12 09:07:37 -0500

    Update lazy

--- lua/plugins_lazy.lua
+++ lua/plugins_lazy.lua
@@ -11,4 +11,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' }
+  },
+  ui = {
+    border = 'rounded'
+  }
+})
