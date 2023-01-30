a9955eadab323ba92d5c9bc81427bcf13cc8bbbc
Luxed <devildead13@gmail.com>
2023-01-30 10:48:41 -0500

    Update plugins + separate things in colorscheme.lua to better debug ayu-vim's performance

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,26 @@
 return {
-  'ayu-vim',
-  url = 'git@github.com:Luxed/ayu-vim.git',
-  branch = 'semantic-tokens-extended-colors',
-  dependencies = {
-    'kyazdani42/nvim-web-devicons',
+  {
+    'ayu-vim',
+    url = 'git@github.com:Luxed/ayu-vim.git',
+    branch = 'semantic-tokens-extended-colors',
+    priority = 1001,
+    config = function()
+      vim.g.ayucolor = 'mirage'
+      vim.g.ayu_italic_comment = true
+      vim.g.ayu_extended_palette = true
+
+      vim.cmd('colorscheme ayu')
+    end,
   },
-  config = function()
-    vim.api.nvim_create_autocmd('ColorScheme', {
-      callback = function()
-        require('colors.post').init()
-      end
-    })
-
-    vim.g.ayucolor = 'mirage'
-    vim.g.ayu_italic_comment = true
-    vim.g.ayu_extended_palette = true
-
-    vim.cmd('colorscheme ayu')
-  end,
-  priority = 1000
+  {
+    'kyazdani42/nvim-web-devicons',
+    config = function()
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        callback = function()
+          require('colors.post').init()
+        end
+      })
+    end,
+    priority = 1000
+  }
 }
