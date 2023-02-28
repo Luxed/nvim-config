28a075b11cbb33ea9fd9dec2e955c1335c45d78f
Luxed <devildead13@gmail.com>
2023-02-28 09:56:06 -0500

    Update plugins + add support for eslint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -199,10 +199,28 @@ return {
     {
       'j-hui/fidget.nvim',
       config = function()
-        require('fidget').setup({})
+        require('fidget').setup({
+          sources = {
+            ['null-ls'] = {
+              ignore = true
+            }
+          }
+        })
       end
     },
     'SmiteshP/nvim-navic',
+    {
+      'jose-elias-alvarez/null-ls.nvim',
+      config = function()
+        local null_ls = require('null-ls')
+        null_ls.setup({
+          sources = {
+            null_ls.builtins.diagnostics.eslint,
+            null_ls.builtins.code_actions.eslint,
+          }
+        })
+      end
+    },
 
     -- LSP installer
     {
