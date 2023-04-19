5b9062d336a7c7c76c494902bf686ce35f0799ac
Luxed <devildead13@gmail.com>
2023-04-19 09:27:47 -0400

    Remove editorconfig plugin when using neovim 0.9 and higher

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -1,5 +1,8 @@
 return {
-  'editorconfig/editorconfig-vim',
+  {
+    'editorconfig/editorconfig-vim',
+    enabled = vim.fn.has('nvim-0.9') == 0 -- Editorconfig support is now built in to Neovim
+  },
   'mboughaba/i3config.vim',
   'PProvost/vim-ps1',
   'blankname/vim-fish',
