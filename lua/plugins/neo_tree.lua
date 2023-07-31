87814d144fd95c9906358743779b946cf613e6ad
Luxed <devildead13@gmail.com>
2023-07-31 17:54:44 -0400

    Add notes for neo_tree

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,3 +1,10 @@
+-- TODO: Once again I'm having issues with my file explorer. It seems like I've never found something that truly works for me and doesn't break all the time.
+-- Here's a new list of requirements:
+-- * Can be opened in the current buffer or in a new split. Opening in the "drawer" mode is completely useless to me.
+-- * Doesn't override too many keys so I can use the file explorer like a read-only buffer.
+-- * Integrates with Git (optional tbh)
+-- * Has file icons (pretty important, it really helps me)
+-- * Doesn't have to be in Lua. At the end of the day, not everything needs to be in Lua. If that means I go back to Fern or NERDTree, then that's fine. I just want something that works.
 return {
   'nvim-neo-tree/neo-tree.nvim',
   dependencies = {
