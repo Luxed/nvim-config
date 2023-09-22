3b2778acc05d0ef4a4a2df172929decc956c77b6
Luxed <devildead13@gmail.com>
2023-09-22 11:19:22 -0400

    Improve go setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,29 +32,29 @@ vim.fn.sign_define('LspDiagnosticsSignInformation', { text = '' })
 vim.fn.sign_define('LspDiagnosticsSignHint', { text = 'ﯦ' })
 vim.fn.sign_define('LightBulbSign', { text = '' })
 
-local function lsp_config(server_name)
-  local complete_lsp_setup = {
-    on_attach = function(client, bufnr)
-      require('commands').lsp()
-      require('keymaps').lsp(client)
+local complete_lsp_setup = {
+  on_attach = function(client, bufnr)
+    require('commands').lsp()
+    require('keymaps').lsp(client)
 
-      if client.server_capabilities.documentSymbolProvider then
-        require('nvim-navic').attach(client, bufnr)
-      end
-    end,
-    capabilities = vim.tbl_deep_extend(
-      'force',
-      vim.lsp.protocol.make_client_capabilities(),
-      require('cmp_nvim_lsp').default_capabilities()
-    )
-  }
+    if client.server_capabilities.documentSymbolProvider then
+      require('nvim-navic').attach(client, bufnr)
+    end
+  end,
+  capabilities = vim.tbl_deep_extend(
+    'force',
+    vim.lsp.protocol.make_client_capabilities(),
+    require('cmp_nvim_lsp').default_capabilities()
+  )
+}
 
-  local function extended_setup(additional_options)
+local function extended_setup(additional_options)
     additional_options = additional_options or {}
 
     return vim.tbl_extend('force', complete_lsp_setup, additional_options)
-  end
+end
 
+local function lsp_config(server_name)
   local lspconfig = require('lspconfig')
   if server_name == 'omnisharp' or server_name == 'omnisharp_mono' then
     lspconfig[server_name].setup(
@@ -115,7 +115,8 @@ local function lsp_config(server_name)
         Lua = {
           completion = {
             callSnippet = "Replace"
-          }
+          },
+          hint = { enable = true }
         }
       }
     }))
@@ -123,6 +124,8 @@ local function lsp_config(server_name)
     lspconfig[server_name].setup(extended_setup({
       root_dir = require('lspconfig.util').root_pattern('deno.json', 'deno.jsonc')
     }))
+  elseif server_name == 'gopls' then
+    -- Do nothing, let the go plugin take care of everything
   else
     lspconfig[server_name].setup(complete_lsp_setup)
   end
@@ -148,6 +151,30 @@ return {
       end
     },
     'simrat39/rust-tools.nvim',
+    {
+      'ray-x/go.nvim',
+      dependencies = {
+        'ray-x/guihua.lua',
+      },
+      config = function()
+        require('go').setup({
+          dap_debug_keymap = false, -- They never get removed from the buffer, super annoying
+          run_in_floaterm = true,
+          floaterm = {
+            posititon = 'right',
+            width = 0.45,
+            height = 0.5, -- height doesn't work with right
+          },
+          lsp_inlay_hints = {
+            enable = true
+          },
+          lsp_cfg = extended_setup(),
+        })
+      end,
+      event = { 'CmdLineEnter' },
+      ft = { 'go', 'gomod' },
+      -- build = ':lua require("go.install").update_all_sync()'
+    },
     {
       'folke/neodev.nvim',
       config = function()
